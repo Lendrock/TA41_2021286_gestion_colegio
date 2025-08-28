c9d82db656b1e97f9a8fe26644430f62ba88a711
Lendrock <lchoxon-2021286@kinal.edu.gt>
2025-08-28 00:43:45 -0600

    Modificacion a la base de datos

--- gestion-colegio.sql
+++ gestion-colegio.sql
@@ -8,10 +8,32 @@ create table Estudiantes(
     correo varchar(64) not null,
     genero enum('masculino', 'femenino', 'no') not null,
     edad integer not null,
+    fechaIncripcion timestamp default current_timestamp,
 
 	constraint pk_Estudiantes primary key (codigoEstudiante)
 );
 
+create table Cursos(
+	codigoCurso int auto_increment,
+    nombreCurso varchar(64) not null,
+    descripcion varchar(255) not null,
+    
+    constraint pk_Cursos primary key (codigoCurso)
+);
+
+create table Inscripciones(
+	codigoInscripcion int auto_increment,
+    codigoEstudiante int not null,
+    codigoCurso int not null,
+    fechaInscripcion timestamp default current_timestamp,
+    
+    constraint pk_Inscripciones primary key (codigoInscripcion),
+    
+    constraint fk_Incripciones_Estudiantes foreign key(codigoEstudiante)
+		references Estudiantes(codigoEstudiante),
+	constraint fk_Incripciones_Cursos foreign key(codigoCurso)
+		references Cursos(codigoCurso)
+);
 insert into Estudiantes(codigoEstudiante, nombreEstudiante, apellidoEstudiante, correo, genero, edad)
 			values(1, "Leandro", "Madonado", "lchoxon-202286@kinal.edu.gt", "no", 50);
             
